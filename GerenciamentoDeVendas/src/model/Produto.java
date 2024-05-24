d748e660b0364793e8beced135d64f92620c767d
Gabriell06 <gabrieldjesus37@gmail.com>
2024-05-23 22:49:53 -0300

    criação da classe produto. metódos e atributos

--- GerenciamentoDeVendas/src/model/Produto.java
+++ GerenciamentoDeVendas/src/model/Produto.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author Ribeiro
- */
 public class Produto {
     
+    
+    private int id;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+   
+    public Produto(int id, String nome, double preco, int quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+   
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+   
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void imprimirInformacoes() {
+        System.out.println("ID: " + id);
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: " + preco);
+        System.out.println("Quantidade: " + quantidade);
+       
+    }
+
 }
+
+
