a78e73c28cb4a5d50a9a3ac8690dd0efe4d2d22f
Gabriel Fernando <gabrielfernandosilvafernandes@gmail.com>
2024-06-15 14:43:13 -0300

    Autenticação do Banco de Dados

---
+++ GerenciamentoDeVendas/src/controller/AutenticacaoDB.java
@@ -0,0 +1,29 @@
+
+package controller;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class AutenticacaoDB {
+    
+    public void cadastrarUsuario(String nome, String senha) throws SQLException {
+        String sql = "INSERT INTO usuarios (nome, senha) VALUES ('"+nome+"','"+senha+"')";
+        Connection conexao = new ConexaoDB().conectar();
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        stmt.execute();
+        conexao.close();
+        
+    }
+    
+    public void logarUsuario(String nome, String senha) throws SQLException {
+        String sql = "SELECT nome,senha FROM usuarios WHERE nome ='"+nome+"' AND senha = '"+senha+"'";
+        Connection conexao = new ConexaoDB().conectar();
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+        conexao.close();
+        
+    }    
+    
+}
--- GerenciamentoDeVendas/src/view/TelaLogin.java
+++ GerenciamentoDeVendas/src/view/TelaLogin.java
@@ -70,9 +70,8 @@ public class TelaLogin extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void usuarioLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_usuarioLoginActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_usuarioLoginActionPerformed
 
+    }//GEN-LAST:event_usuarioLoginActionPerformed
     private void botaoConectarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botaoConectarMouseClicked
        String nome = usuarioLogin.getText();
        String senha = new String(senhaLogin.getPassword());
